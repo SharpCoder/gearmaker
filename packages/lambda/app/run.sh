8b87027cea1920919197abb5137396a06aedc3ff
Josh Cole <colemangrill@live.com>
2022-03-21 15:18:30 -0700

    blah

--- packages/lambda/app/run.sh
+++ packages/lambda/app/run.sh
@@ -6,6 +6,9 @@ INPUT=$1
 # Second parameter is file output
 OUTPUT=$2
 
+# Kill al Xvfb processes
+ps aux | grep Xvfb | awk '{system("kill "$2)}'
 rm -rf /tmp/.X99-lock 
+
 export DISPLAY=:99
 Xvfb :99 -screen 0 800x600x24 -once& openscad -i $OUTPUT --colorscheme DeepOcean $INPUT
